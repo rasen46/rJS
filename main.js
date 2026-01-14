a70b2d8f23d5f2fd1abeb422bd69156ca15fb878
rasen46 <136291290+rasen46@users.noreply.github.com>
2026-01-13 19:18:01 -0500

    Update rasenJS resources version and improve error handling

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var rjs = {
 	startTime: getTime(),
 	modules: {},
 
-	// rasenJS resources v1.0.1 real //
+	// rasenJS resources v1.0.2 real //
 	// source file: https://github.com/rasen46/rJS //
 	print: function(text, type) {
 		var cTime = getTime() - rjs.startTime;
@@ -20,7 +20,7 @@ var rjs = {
 		var logType = (type ? typeof type == "string" ? type.toUpperCase() : "INFO" : "INFO");
 		console.log("[" + formatToReadable + "] " + "[script.js/" + logType + "]: " + text);
 	},
-	
+
 	atob: function(str) {
 		if (!str || typeof str != "string") return rjs.print("got malformed string (is it a string?)", "WARN");
 		//used a tutorial since i idk how base64 works
@@ -51,7 +51,9 @@ var rjs = {
 				var usableData = JSON.parse(content),
 					b64 = usableData.content,
 					fName = usableData.name;
-				rjs.modules[fName] = eval(rjs.atob(b64));
+				if (!rjs.atob(b64) || !fName) {
+					return rjs.print("failed to load module due to invaild base64 or fileName", "WARN");
+				} else rjs.modules[fName] = eval(rjs.atob(b64));
 			} else {
 				return rjs.print("failed to load module: " + status, "WARN");
 			}
@@ -61,21 +63,22 @@ var rjs = {
 	require: function(name, callback, max) {
 		if (!name || typeof name != "string") return rjs.print("got malformed name (is it a string?)", "WARN");
 
-		var maxAttempts = max || 5;
-		var i = 0;
-		var tl;
+		var maxAttempts = Math.abs(max) || 5,
+			i = 0,
+			required;
 
-		tl = timedLoop(100, function() {
-			i++;
-
-			if (rjs.modules[name]) {
+		var tl = timedLoop(100, function() {
+			if (required) {
 				stopTimedLoop(tl);
+			} else if (typeof rjs.modules[name] == "object") {
 				callback(rjs.modules[name]);
-			} else if (i > maxAttempts) {
-				stopTimedLoop(tl);
-				callback(undefined);
+				required = true;
+			} else if (i >= maxAttempts) {
 				rjs.print("took too long to retrieve requested module: " + name, "WARN");
+				callback(undefined);
+				required = true;
 			}
+			i++;
 		});
 	}
 };
